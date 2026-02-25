2efd66636079936a5f7da8c97cdc3edbf066e67b
JuanSilvaaa <juan.silva-c@mail.escuelaing.edu.co>
2026-02-25 10:44:13 -0500

    organizacion de carpetas

--- DOSW-ParcialT1/DOSW-ParcialT1/src/main/java/edu/dosw/parcial/App.java
+++ DOSW-ParcialT1/DOSW-ParcialT1/src/main/java/edu/dosw/parcial/App.java
@@ -1,13 +1,12 @@
-package edu.dosw.parcial.DOSW_ParcialT1;
+package edu.dosw.parcial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DoswParcialT1Application {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DoswParcialT1Application.class, args);
+		SpringApplication.run(App.class, args);
 	}
-
 }
--- DOSW-ParcialT1/DOSW-ParcialT1/test/java/edu/dosw/parcial/AppTest.java
+++ DOSW-ParcialT1/DOSW-ParcialT1/test/java/edu/dosw/parcial/AppTest.java
@@ -1,13 +1,12 @@
-package edu.dosw.parcial.DOSW_ParcialT1;
+package edu.dosw.parcial;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DoswParcialT1ApplicationTests {
+class AppTest {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
